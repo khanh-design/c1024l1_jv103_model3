78fdb351efcad900bf3c7561908bd7cfc19cf6a3
Khanh-desgin <buiduykhanh0162003@gmail.com>
2025-03-13 01:57:48 +0700

    first commit

--- Lession01/BaiTap/ThucHanh_3.sql
+++ Lession01/BaiTap/ThucHanh_3.sql
@@ -8,3 +8,16 @@ create table Student(
     age int,
     country varchar(50)
 );
+
+# tiep bai tap tiep theo
+create table Class (
+	id int primary key,
+    name varchar(20)
+);
+
+create table Teacher(
+	id int primary key,
+    name varchar(200),
+    age int,
+    country varchar(50)
+);
